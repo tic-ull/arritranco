a06e4379e966858fbb72167b11372748c1eac039
esicilia <esicilia@ull.es>
2010-07-26 13:33:18 +0100

    Including the admin interface

--- arritranco/settings.py
+++ arritranco/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django_extensions',
     'south',
 )
