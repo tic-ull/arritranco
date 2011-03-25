b00995684947dcea4dc0949ded05c19f3da77c73
esicilia <esicilia@ull.es>
2011-03-25 08:47:08 +0000

    Some changes related to models

--- arritranco/hardware/admin.py
+++ arritranco/hardware/admin.py
@@ -4,9 +4,14 @@ Created on 23/12/2010
 @author: esauro
 '''
 from django.contrib import admin
-from models import PhysicalServer, Switch, Rack
+from models import PhysicalServer, Switch, Rack, Manufacturer, HwModel, HwType, RackPlace, Chasis, BladeServer
 
 admin.site.register(PhysicalServer)
 admin.site.register(Switch)
 admin.site.register(Rack)
-
+admin.site.register(Manufacturer)
+admin.site.register(HwModel)
+admin.site.register(HwType)
+admin.site.register(RackPlace)
+admin.site.register(Chasis)
+admin.site.register(BladeServer)
