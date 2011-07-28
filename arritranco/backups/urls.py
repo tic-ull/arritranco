5e9378beb51069ae8f909bf13fd0b2c11241101f
René Martín Rodríguez <rmrodri@ull.es>
2011-07-28 13:45:00 +0100

    backup.url missing

---
+++ arritranco/backups/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls.defaults import patterns, url
+from djangorestframework.resources import ModelResource
+from djangorestframework.views import ListOrCreateModelView, InstanceModelView
+from models import BackupTask, TSMBackupTask, R1BackupTask
+from scheduler.models import Task
+from scheduler.views import Todo
+
+class BackupTaskResource(ModelResource):
+    model = BackupTask
+
+class BackupsTodo(Todo):
+    queryset = BackupTask.objects.all()
+
+class TSMBackupsTodo(BackupsTodo):
+    queryset = TSMBackupTask.objects.all()
+
+class R1BackupsTodo(BackupsTodo):
+    queryset = R1BackupTask.objects.all()
+
+urlpatterns = patterns('',
+    url(r'^$', ListOrCreateModelView.as_view(resource=BackupTaskResource)),
+    url(r'^tsm/todo/$', TSMBackupsTodo.as_view(), name='tsm-backups-todo'), 
+    url(r'^r1/todo/$', R1BackupsTodo.as_view(), name='r1-backups-todo'), 
+    url(r'^todo/$', BackupsTodo.as_view(), name='backups-todo'), 
+    url(r'^(?P<pk>[^/]+)/$', InstanceModelView.as_view(resource=BackupTaskResource)),
+)
