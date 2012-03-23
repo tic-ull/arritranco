cfc86e6f530762f0dd0a792106442ffa7ca2cc01
René Martín Rodríguez <rmrodri@ull.es>
2012-03-23 12:35:14 +0000

    Improved admin interface

--- arritranco/backups/admin.py
+++ arritranco/backups/admin.py
@@ -19,6 +19,7 @@ class FileBackupTaskAdmin(admin.ModelAdmin):
     list_display = ('machine', 'duration', 'bckp_type', 'checker_fqdn', 'directory', 'days_in_hard_drive', 'max_backup_month')
     list_filter = ('bckp_type', 'checker_fqdn')
     list_editable = ('bckp_type', )
+    search_fields = ['machine__fqdn', 'directory', 'checker_fqdn', 'description']
     inlines = [FileBackupProductInline, ]  
 
 admin.site.register(FileBackupTask, FileBackupTaskAdmin)
