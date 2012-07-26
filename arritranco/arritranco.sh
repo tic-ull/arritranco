5651b01bc50d6d3de53800508bd6f2396065fa96
Adrián Santos Marrero <asmarre@ull.es>
2012-07-26 17:01:14 +0100

    Removing some commented out code

--- arritranco/arritranco.sh
+++ arritranco/arritranco.sh
@@ -5,7 +5,4 @@ pwd
 test -e ../virtualenv && VIRTUALENV=../virtualenv
 test -e virtualenv && VIRTUALENV=virtualenv
 source $VIRTUALENV/bin/activate
-#export PYTHONPATH=$PYTHONPATH:$(pwd)
-#$VIRTUALENV/bin/python manage.py release_sem
-#DJANGO_SETTINGS_MODULE=arritranco.settings
 exec $VIRTUALENV/bin/gunicorn_django $*
