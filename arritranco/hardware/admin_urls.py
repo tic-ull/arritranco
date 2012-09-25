fe1d26f76c4a526a121fba3ea81a04501750177f
René Martín Rodríguez <rmrodri@ull.es>
2012-09-25 11:13:23 +0100

    Enable filter by room and rack in rack_view

--- arritranco/hardware/admin_urls.py
+++ arritranco/hardware/admin_urls.py
@@ -4,5 +4,7 @@ from hardware.admin_views import RoomRackView
 
 urlpatterns = patterns('',
     url(r'^room-rack$', RoomRackView.as_view(), name="room-rack-view"),
+    url(r'^room-rack/(?P<room_slug>[-\w]+)$', RoomRackView.as_view(), name="room-rack-view"),
+    url(r'^room-rack/(?P<room_slug>[-\w]+)/(?P<rack_slug>[-\w]+)$', RoomRackView.as_view(), name="room-rack-view"),
 )
 
--- arritranco/hardware/admin_views.py
+++ arritranco/hardware/admin_views.py
@@ -8,6 +8,9 @@ from location.models import Room
 class RoomRackView(TemplateView):
     template_name = "admin/hardware/rack/room-rack.html"
 
-    def get_context_data(self):
-        return { 'rooms': Room.objects.all().order_by('building'), }
+    def get_context_data(self, room_slug = None, rack_slug = None):
+        qs = Room.objects.all()
+        if room_slug:
+            qs = qs.filter(slug = room_slug)
+        return { 'rooms': qs, 'rack_slug': rack_slug}
 
