9ace97995422a911b053036e7c1e083f323ea6c7
René Martín Rodríguez <rmrodri@ull.es>
2012-09-28 14:23:20 +0100

    Corrections ..

--- arritranco/inventory/admin_views.py
+++ arritranco/inventory/admin_views.py
@@ -22,7 +22,7 @@ class UpdateListView(ListView):
     def get_context_data(self, object_list):
         one_month_ago = datetime.date.today() - datetime.timedelta(days = 30)
         return {
-                'one_month_ago': one_month_ago,
+                'two_month_ago': one_month_ago,
                 'three_month_ago': one_month_ago - datetime.timedelta(days = 2 * 30),
                 'sisx_month_ago': one_month_ago - datetime.timedelta(days = 5 * 30),
                 'object_list': object_list.filter(Q(up_to_date_date__lte = one_month_ago) | Q(up_to_date_date__isnull = True))
