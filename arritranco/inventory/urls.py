180e2b12aa596f5ef72c53e585647c9cf4169c19
Adrián Santos Marrero <asmarre@ull.es>
2013-05-13 11:50:28 +0100

    Adding a view to update the up_to_date_date machine field.

---
+++ arritranco/inventory/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import patterns, url
+
+from views import *
+
+urlpatterns = patterns('',
+    url(r'^update_update_date/(?P<machine_id>\d+)$', update_update_date, name="update_update_date"),
+)
