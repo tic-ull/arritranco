7cb8f83fa3c67a845941360ed10b86b9d28adee2
Adrián Santos Marrero <asmarre@ull.es>
2013-05-13 11:50:28 +0100

    Adding a view to update the up_to_date_date machine field.

--- arritranco/inventory/views.py
+++ arritranco/inventory/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+
+@permission_required('change_machine')
+def update_update_date(request, machine_id):
+    m = Machine.objects.get(pk = machine_id)
+    m.up_to_date_date = datetime.datetime.today()
+    m.save()
+    return HttpResponseRedirect(reverse('update-list'))
+
