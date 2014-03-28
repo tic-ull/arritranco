72a3adc01608a43a57e4ab321b27e2dc61cd0e59
Sistemas <sistemas@ull.es>
2014-03-28 09:34:00 +0000

    Adding filter to VirtualMachine query in MachinesUPSProperty view

--- arritranco/backups/models.py
+++ arritranco/backups/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from arritranco.scheduler.models import Task, TaskCheck, TaskManager
+from scheduler.models import Task, TaskCheck, TaskManager
 from inventory.models import Machine
 from django.conf import settings
 
