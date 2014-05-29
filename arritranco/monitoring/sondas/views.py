c9086bf6d4a3fd270a3180eecdbfc4509a5902a0
Alberto Lorenzo <ocholetrasaleatorias@gmail.com>
2014-05-29 11:44:42 +0100

    Added services on nagios sondas cfg view

--- arritranco/monitoring/sondas/views.py
+++ arritranco/monitoring/sondas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from rest_framework.views import Response, APIView
 from django.template import Context
+from monitoring.nagios.models import Service
 from monitoring.sondas.models import Sonda, NagiosNrpeCheckOpts
 
 
@@ -14,6 +15,7 @@ class NrpeCfg(APIView):
                                       Context({
                                           "NrpeCheckOpts": NagiosNrpeCheckOpts.objects.all(),
                                           "sondas": Sonda.objects.all(),
+                                          "services": Service.objects.all(),
                                       }),
                                       mimetype="text/plain")
         response['Content-Disposition'] = 'attachment; filename=nagios.cfg'
