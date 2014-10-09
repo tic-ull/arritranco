847c30c803be7045eeb66f1ba25410471bace59c
ahercon <ahercon@ull.es>
2014-10-09 15:16:51 +0100

    Change celery

--- arritranco/backups/tasks.py
+++ arritranco/backups/tasks.py
@@ -5,34 +5,29 @@ from kombu import  Queue
 from django.conf import settings
 import socket
 from backups.scripts.stic_check import  compress_Checker, deletefile_Checker, verifybackup_Checker
-from arritranco.celery import app
 
-#app = Celery(settings.BACKUP_QUEUENAME, broker=settings.BROKER_URL)
+app = Celery(settings.BACKUP_QUEUENAME, broker=settings.BROKER_URL)
 if socket.gethostname() == settings.CHECKER:
     """ im the worker """
     queue = []
     queue.append(Queue(settings.CHECKER, routing_key=settings.CHECKER))
     app.conf.CELERY_QUEUES = queue
 
-@app.task(ignore_result=True)
+@app.task()
 def verify_backupfile(fqdn, id, directory):
     """ verify the backupfiletask """
     return True
 
-<<<<<<< HEAD
 @app.task()
 def compress_backupfile(filename,backup_id,directory,fqdn,id):
-=======
-@app.task(ignore_result=True)
-def compress_backupfile(filename,backup_id,directory,fqdn, id):
->>>>>>> 402dd460d88f5d860e9e42b82bdf5c3ca673df17
     """ compress backupfile """
     compress_Checker(backup_id,directory + filename)
     verifybackup_Checker(fqdn, id, directory)
     return True
 
-@app.task(ignore_result=True)
+@app.task()
 def delete_backupfile(filename,fqdn):
     """ delete backupfile """
     deletefile_Checker(filename)
     return True
+
