9881fd1d0174d1d828c5790c05637c41bfa563e2
ahercon <ahercon@ull.es>
2014-10-22 08:27:29 +0100

    Change oracle backup directory

--- arritranco/backups/views.py
+++ arritranco/backups/views.py
@@ -69,9 +69,11 @@ def verify_backup_on_checker(filename,fbp, bid):
         aux = fbt.extra_options.replace('data=','').split(':')
         directory = "/backup/" + fqdn + "/dump-" + aux[0] + "/" + aux[1] + "/"
         if fbt.extra_options.find("data=oracle_export:") == 0:
-             directory = "/backup/" + fqdn + "/filebackup/"
+             db = fbt.extra_options.split(":")[1]
+             directory = "/backup/" + fqdn + "/filebackup/" + db + "/"
         if fbt.extra_options.find("data=oracle_rman:") == 0:
-             directory = "/backup/" + fqdn + "/rman/"
+             db = fbt.extra_options.split(":")[1]
+             directory = "/backup/" + fqdn + "/rman/" + db + "/"
     res = compress_backupfile.apply_async((filename,bid, directory, fqdn, id), serializer="json", queue=checker, ignore_result=True, countdown=120)
 
 def add_backup_file(request, machine=None, windows=False):
