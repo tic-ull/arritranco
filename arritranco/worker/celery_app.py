ea888fb4a12f0430cdb2994f70040578cf01d993
ahercon <ahercon@ull.es>
2014-10-28 09:45:50 +0000

    Load celery configuration from django.conf:settings

--- arritranco/worker/celery_app.py
+++ arritranco/worker/celery_app.py
@@ -1,19 +1,11 @@
 from __future__ import absolute_import
-
 import os
-
 from celery import Celery
-
 from django.conf import settings
 
 
-app = Celery(settings.BACKUP_QUEUENAME, broker=settings.BROKER_URL)
-
+app = Celery('backup_task')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-#app.config_from_object('django.conf:settings')
-#app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
