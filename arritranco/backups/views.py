47baf98a27f6209382823e781a7d6eea077a7003
ahercon <ahercon@ull.es>
2014-10-31 09:11:06 +0000

    Remove de directory backup calc

--- arritranco/backups/views.py
+++ arritranco/backups/views.py
@@ -18,7 +18,7 @@ import math
 import os
 import logging
 import time
-from worker.tasks import verify_backupfile, delete_backupfile, compress_backupfile
+from worker.tasks import verify_backupfile
 
 
 logger = logging.getLogger(__name__)
@@ -78,21 +78,8 @@ def verify_backup_on_checker(filename,fbp, bid):
     checker = fbt.checker_fqdn
     fqdn = fbt.machine.fqdn
     id = str(fbt.id)
+    res = verify_backupfile.apply_async((filename,bid, fqdn, id), serializer="json", queue=checker, ignore_result=True, countdown=120)
 
-    if fbt.extra_options == "":
-        directory = "/backup/" + fqdn + "/dumps/"
-    else:
-        aux = fbt.extra_options.replace('data=','').split(':')
-        directory = "/backup/" + fqdn + "/dump-" + aux[0] + "/" + aux[1] + "/"
-        if fbt.extra_options.find("data=oracle_dump:") == 0:
-             opts = fbt.extra_options.splitlines(True)[0]
-             db = opts.split(":")[1].rstrip()
-             directory = "/backup/" + fqdn + "/filebackup/" + db + "/"
-        if fbt.extra_options.find("data=oracle_rman:") == 0:
-             opts = fbt.extra_options.splitlines(True)[0]
-             db = opts.split(":")[1].rstrip()
-             directory = "/backup/" + fqdn + "/rman/" + db + "/"
-    res = compress_backupfile.apply_async((filename,bid, directory, fqdn, id), serializer="json", queue=checker, ignore_result=True, countdown=120)
 
 def add_backup_file(request, machine=None, windows=False):
     """Add a file to a backup task."""
